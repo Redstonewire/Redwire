5a7a6259f4905f8500ccf262d3945b490cbc0b19
Vandalite <Private@Private.name>
2013-05-17 01:45:51 -0400

    Complete overhaul of naming scheme
    Classes now have Capitalized names, objects inside are camelCase named.
    Also cleaned up the code a bit.

--- Redwire_Common/com/redwire/lib/reference.java
+++
@@ -1,8 +0,0 @@
-package com.redwire.lib;
-
-public class reference
-{
-    public static final String MOD_ID = "RW";
-    public static final String MOD_NAME = "Redwire";
-    public static final String VERSION = "0.0.1preA";
-}
